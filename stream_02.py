791d338d0d37469147b703d8ec049917f0ded276
finanzascuantitativasfc <69920097+finanzascuantitativasfc@users.noreply.github.com>
2020-09-25 16:13:38 +0100

    Add files via upload

--- stream_02.py
+++ stream_02.py
@@ -16,26 +16,26 @@ from scipy.stats import skew, kurtosis, chi2
 '''
 Goal: create a normality test e.g. Jarque-Bera
 
-step 1: generate random variables
-step 2: visualise histogram
-step 3: Jarque-Bera test
-step 4: p-value
+step 1: generate vector of iid random variables
+step 2: visualise histogram of random variables
+step 3: define what is the p-value
+step 4: perform a Jarque-Bera test
 step 5: a normal distribution can fail its normality test
 
 To run the while loop of the normality test until it fails:
 * Uncomment lines 31-33 and 80-82
-* Indent lines 35-78 (select them and press Tab; to undo indent press Shift+Tab)
+* Indent lines 35-78 (select them and press Tab)
 
 '''
-# playing with Borel-Cantelli: P[all tests are normal = True] = 0
+### playing with Borel-Cantelli: P[all tests are normal = True] = 0
 # is_normal = True
 # counter = 0
-# while is_normal:
+# while is_normal and counter < 50:
     
 # generate random variable
 x_size = 10**6
-degrees_freedom = 5000
-type_random_variable = 'normal' # normal exponential student
+degrees_freedom = 2
+type_random_variable = 'normal' # normal exponential student chi-squared
 
 if type_random_variable == 'normal':
     x = np.random.standard_normal(size=x_size)
@@ -56,6 +56,7 @@ x_stdev = np.std(x)
 x_skew = skew(x)
 x_kurt = kurtosis(x) # excess kurtosis
 x_var_95 = np.percentile(x,5)
+x_cvar_95 = np.mean(x[x <= x_var_95])
 jb = x_size/6*(x_skew**2 + 1/4*x_kurt**2)
 p_value = 1 - chi2.cdf(jb, df=2)
 is_normal = (p_value > 0.05) # equivalently jb < 6
@@ -67,18 +68,19 @@ print('std ' + str(x_stdev))
 print('skewness ' + str(x_skew))
 print('kurtosis ' + str(x_kurt))
 print('VaR 95% ' + str(x_var_95))
+print('CVaR 95% ' + str(x_cvar_95))
 print('Jarque-Bera ' + str(jb))
 print('p-value ' + str(p_value))
 print('is normal ' + str(is_normal))
-
+    
 # plot histogram
 plt.figure()
 plt.hist(x,bins=100)
 plt.title('Histogram ' + x_str)
 plt.show()
     
-    # print('counter ' + str(counter))
     # counter +=1
+    # print('counter ' + str(counter))
     # print('-----')
 
 
